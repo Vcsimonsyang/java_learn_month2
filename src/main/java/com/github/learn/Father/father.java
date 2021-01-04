9925e1ccdfc30bb3e94f39bf82a13394b5a5cbb9
lynnyanggod <ouyangyly@gmail.com>
2021-01-04 23:22:28 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/github/learn/Father/father.java
@@ -0,0 +1,7 @@
+package com.github.learn.Father;
+
+public class father {
+    public int age = 40;
+    public String name = "Yang";
+
+}
---
+++ src/main/java/com/github/learn/Father/son.java
@@ -0,0 +1,11 @@
+package com.github.learn.Father;
+
+public class son extends father {
+    public int age = 20;
+    public String name = "Yang_junior";
+
+    public void show(){
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
