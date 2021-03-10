51849aeabb757569ccb2d3b29857748493d535da
lynnyanggod <ouyangyly@gmail.com>
2021-03-10 23:57:05 +0800

    test TCP/IP and Thread

--- src/main/java/com/github/learn/TCPIP/ClientDemo1.java
+++ src/main/java/com/github/learn/TCPIP/ClientDemo1.java
@@ -1,4 +1,14 @@
 package com.github.learn.TCPIP;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class ClientDemo1 {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.1.6",10086);
+        OutputStream os = s.getOutputStream();
+        byte[] bys = "this is another test".getBytes();
+
+    }
 }
